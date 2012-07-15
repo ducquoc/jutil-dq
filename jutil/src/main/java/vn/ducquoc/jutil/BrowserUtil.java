69548b5421dd7aec3e532b13db6a0ad3171c4d9e
ducquoc <ducquoc.vn@gmail.com>
2012-07-15 15:03:55 +0700

    minor refactoring

--- jutil/src/main/java/vn/ducquoc/jutil/BrowserUtil.java
+++ jutil/src/main/java/vn/ducquoc/jutil/BrowserUtil.java
@@ -1,22 +1,20 @@
 package vn.ducquoc.jutil;
 
-import java.lang.reflect.Method;
-
-import javax.swing.JOptionPane;
-
 /**
- * Helper class for invoking browser.
+ * Helper class for opening browser and retrieving related information.
  * 
  * @author ducquoc
- * @see org.apache.commons.lang.StringUtils
- * @see org.mortbay.util.StringUtil
+ * @see com.intellij.ide.BrowserUtil
+ * @see org.apache.uima.internal.util.BrowserUtil
+ * @see com.centerkey.utils.BareBonesBrowserLaunch
+ * @see weka.gui.BrowserHelper
  */
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class BrowserUtil {
 
-    private static final String BROWSER_NOT_FOUND = "Web browser not found \n";
+    private static final String BROWSER_NOT_FOUND_ERROR = "[ERROR] Web browser not found \n";
 
-    private static final String BROWSER_ERROR = "Error attempting to launch web browser: \n";
+    private static final String BROWSER_ERROR = "[ERROR] failed to launch web browser: \n";
 
     private static final String LAUNCHING_DEFAULT_BROWSER = "[INFO] Launching default browser...";
 
@@ -28,34 +26,40 @@ public class BrowserUtil {
     public static void openUrl(String url) {
         String osName = System.getProperty("os.name");
         try {
-            if (osName.startsWith("Mac OS")) { // Safari
+            System.out.println(LAUNCHING_DEFAULT_BROWSER);
+            if (osName.startsWith("Mac OS")) {
+                // Runtime.getRuntime().exec("open " + url);
+
                 Class fileMgr = Class.forName("com.apple.eio.FileManager");
-                Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[] { String.class });
+                java.lang.reflect.Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[] { String.class });
                 openURL.invoke(null, new Object[] { url });
 
             } else if (osName.startsWith("Windows")) {
-                System.out.println(LAUNCHING_DEFAULT_BROWSER);
                 Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
 
             } else { // Linux or Unix
-                String[] browsers = { "google-chrome", "firefox", "opera", "konqueror", "safari", "epiphany", "mozilla" };
+                String[] browsers = { "google-chrome", "firefox", "opera", "konqueror", "epiphany", "mozilla", "links" };
                 String browser = null;
                 for (int count = 0; count < browsers.length && browser == null; count++)
                     if (Runtime.getRuntime().exec(new String[] { "which", browsers[count] }).waitFor() == 0)
                         browser = browsers[count];
                 if (browser == null)
-                    throw new RuntimeException(BROWSER_NOT_FOUND);
+                    throw new RuntimeException(BROWSER_NOT_FOUND_ERROR);
                 else
                     Runtime.getRuntime().exec(new String[] { browser, url });
             }
         } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, BROWSER_ERROR + ex.getMessage());
+            // System.out.println(BROWSER_ERROR + ex.getMessage());
+            javax.swing.JOptionPane.showMessageDialog(null, BROWSER_ERROR + ex.getMessage());
+
+            // java.awt.Desktop.getDesktop().browse(new URI(url)); // JDK6+
         }
     }
 
     public static void main(String args[]) {
-        openUrl("www.google.com");
-        // openUrl("www.wikipedia.org");
+        String url = (args.length == 0) ? "www.google.com" : args[0];
+
+        openUrl(url);
     }
 
 }
--- jutil/src/test/java/vn/ducquoc/jutil/HealthcareUtilTest.java
+++ jutil/src/test/java/vn/ducquoc/jutil/HealthcareUtilTest.java
@@ -1,5 +1,7 @@
 package vn.ducquoc.jutil;
 
+import org.junit.Assert;
+
 /**
  * Tests for <code>vn.ducquoc.jutil.HeathcareUtil</code>.
  */
@@ -7,71 +9,73 @@ public class HealthcareUtilTest {
 
     @org.junit.Test
     public void testValidNpi() {
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidNpi("1234567893"));
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidNpi("1987654328"));
+        Assert.assertTrue(HealthcareUtil.isValidNpi("1234567893"));
+        Assert.assertTrue(HealthcareUtil.isValidNpi("1987654328"));
 
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("1234567890"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("1987654320"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("1234567890"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("1987654320"));
     }
 
     @org.junit.Test
     public void testValidNpi_WithPrefix() {
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidNpi("808401234567893"));
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidNpi("808401987654328"));
+        Assert.assertTrue(HealthcareUtil.isValidNpi("808401234567893"));
+        Assert.assertTrue(HealthcareUtil.isValidNpi("808401987654328"));
 
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("808401234567890"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("808401987654320"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("808401234567890"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("808401987654320"));
     }
 
     @org.junit.Test
     public void testValidNpi_CornerCases() {
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidNpi("0000000006"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("0000000000"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("00000000006"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("000000006"));
+        Assert.assertTrue(HealthcareUtil.isValidNpi("0000000006"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("0000000000"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("00000000006"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("000000006"));
 
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidNpi("0000000014"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("00000000144"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("00000000014"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("000000014"));
+        Assert.assertTrue(HealthcareUtil.isValidNpi("0000000014"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("00000000144"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("00000000014"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("000000014"));
 
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("0123456789"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidNpi("0"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("0123456789"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("06"));
+        Assert.assertFalse(HealthcareUtil.isValidNpi("0"));
     }
 
     @org.junit.Test
     public void testValidDea() {
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidDea("1234563"));
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidDea("1987657"));
+        Assert.assertTrue(HealthcareUtil.isValidDea("1234563"));
+        Assert.assertTrue(HealthcareUtil.isValidDea("1987657"));
 
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("1234560"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("1987650"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("1234560"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("1987650"));
     }
 
     @org.junit.Test
     public void testValidDea_WithPrefix() {
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidDea("AM1234563"));
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidDea("FP1987657"));
+        Assert.assertTrue(HealthcareUtil.isValidDea("AM1234563"));
+        Assert.assertTrue(HealthcareUtil.isValidDea("FP1987657"));
 
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AM1234560"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("FP1987650"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AM1234560"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("FP1987650"));
     }
 
     @org.junit.Test
     public void testValidDea_CornerCases() {
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidDea("0000000"));
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidDea("AA0000000"));
-        org.junit.Assert.assertTrue(HealthcareUtil.isValidDea("AA0000012"));
+        Assert.assertTrue(HealthcareUtil.isValidDea("0000000"));
+        Assert.assertTrue(HealthcareUtil.isValidDea("AA0000000"));
+        Assert.assertTrue(HealthcareUtil.isValidDea("AA0000012"));
 
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA0000001"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA00000000"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA00000001"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA00000010"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA00000012"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA000000"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA000001"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA000010"));
-        org.junit.Assert.assertFalse(HealthcareUtil.isValidDea("AA000012"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA0000001"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA00000000"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA00000001"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA00000010"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA00000012"));
+
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA000000"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA000001"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA000010"));
+        Assert.assertFalse(HealthcareUtil.isValidDea("AA000012"));
     }
 
 }
